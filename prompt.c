c1919f372fcbb899dc514763106c3de9b26d9ca2
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-08-29 17:57:10 +0100

    merger: prompt & strtok

--- prompt.c
+++ prompt.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 
 int main(void)
 {
 	size_t n;
-	char *user_input = NULL;
-	ssize_t num_chars;
+	char *user_input = NULL; /*char pointer holding user input*/
+	ssize_t num_chars;		 /*return val for getline-func*/
+	char *token;			 /*return val for strtok-func*/
+	int num_token = 0;		 /*number of tokens*/
+	char *delim = " \n";	 /*space or a new-line as delimiter*/
 
 	printf("$ ");
 	/*to read from standdard input we pass stdin as FILE stream argument*/
@@ -15,6 +19,13 @@ int main(void)
 	if (num_chars != -1)
 	{
 		printf("%s\n", user_input);
+		token = strtok(user_input, delim);
+		while (token != NULL)
+		{
+			printf("%s\n", token);
+			num_token++;
+			token = strtok(NULL, delim);
+		}
 		/*printf("Number of bytes: %d\n", num_chars);*/
 	}
 	return (0);
