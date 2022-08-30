828c5d9ab7ac69cec33f600b26e9496e57a17939
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2022-08-30 01:57:19 +0100

    parsing arguments

--- prompt.c
+++ prompt.c
@@ -7,14 +7,19 @@ int main(void)
 {
 	size_t n;
 	char *user_input = NULL; /*char pointer holding user input*/
+	char *usr_in = NULL;	 /*copy of user input*/
 	ssize_t num_chars;		 /*return val for getline-func*/
 	char *token;			 /*return val for strtok-func*/
-	int num_token = 0;		 /*number of tokens*/
-	char *delim = " \n";	 /*space or a new-line as delimiter*/
+	int num_token = 1;		 /*number of tokens set as 1 for NULL byte*/
+	int i = 0;
+	char *delim = " \n"; /*space or a new-line as delimiter*/
+	char **av;
 
 	printf("$ ");
 	/*to read from standdard input we pass stdin as FILE stream argument*/
 	num_chars = getline(&user_input, &n, stdin);
+	/*copy the string passed in by user for safe keeping*/
+	stpcpy(usr_in, user_input);
 	/*num_chars would be -1 if getline fails*/
 	if (num_chars != -1)
 	{
@@ -26,7 +31,19 @@ int main(void)
 			num_token++;
 			token = strtok(NULL, delim);
 		}
-		/*printf("Number of bytes: %d\n", num_chars);*/
+		printf("Number of arguments: %d\n", num_token);
+		/*allocating memory space for the argument array ptrs*/
+		av = malloc(num_token * sizeof(char *));
+		/*add each argument parsed into the array av*/
+		token = strtok(usr_in, delim);
+		while (token != NULL)
+		{
+			/*allocating memory space for the each argument*/
+			av[i] = malloc(sizeof(char) * strlen(token));
+			i++;
+			token = strtok(NULL, delim);
+		}
+		av[i] = malloc(sizeof(char) * strlen(token));
 	}
 	return (0);
 }
