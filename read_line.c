0653a041ee9a2bd693c62b292e5898bde9114fb5
Mcdonald <mcdonaldsamure91@gmail.com>
2022-09-07 10:01:30 -0700

    Updating Shell

---
+++ read_line.c
@@ -0,0 +1,16 @@
+#include "shell.c"
+
+/**
+ * read_line - reads the input string.
+ * @i_eof: returns value of getline function
+ * Return: input string.
+ */
+char *read_line(int *i_eof)
+{
+	char *entry = NULL;
+	size_t bufsize = 0;
+
+	*i_eof = getline(&entry, &bufsize, stdin);
+
+	return (input);
+}
